fd6b70f7efe409760f0e14f9a8c99f1cdc944dc2
Ben-Weber <benfweber@gmail.com>
2021-06-30 16:24:14 +0300

    Project-Start-25min

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; // importing React
+import ReactDOM from "react-dom"; // important React DOM
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
+  // grabbing div (w/ #root) & inserting our App into that div (this App comes from ./App.js line 4 "which contains the root App Component". App is the Root Component, which everything goes under it)
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
