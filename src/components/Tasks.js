ba91e0948cb3f3d6ef552802badffbb799f155ea
Ben-Weber <benfweber@gmail.com>
2021-07-01 13:30:39 +0300

    State & Hook

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,14 +1,11 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "Feb 5th at 2:30pm",
-    reminder: true,
-  },
-];
-
-const Tasks = () => {
-  return <div></div>;
+const Tasks = ({ tasks }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3 key={task.id}> {task.text} </h3>
+      ))}
+    </>
+  );
 };
 
 export default Tasks;
